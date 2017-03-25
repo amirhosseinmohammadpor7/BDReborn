34b1ad33c7a8ff7f0f91da4abc487208630f55af
amirhosseinmohammadpor7 <amirhosseinmohammadpor7@gmail.com>
2017-03-25 18:02:59 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
 		"fun",
 		"info"
 	},
-    sudo_users = {157059515},
+    sudo_users = {301216782},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
